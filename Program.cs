194209628cba41b92157a3797cba0a5ad93feef1
jayukasode23 <jayukasode2398@gmail.com>
2022-12-15 12:38:37 +0530

    Added Retrive Employee Paayroll

--- Program.cs
+++ Program.cs
@@ -7,22 +7,22 @@ namespace EmployeePyroll
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-------Welcome To EmployeePayroll Problem in ADO.Net-------");
+            Console.WriteLine("Welcome to the EmployeePayroll porblem in ADO.Net");
             EmployeeRepo employeeRepo = new EmployeeRepo();
 
             EmployeeModel employeeModel = new EmployeeModel();
-            employeeModel.EmployeeName = "Bruce Wayne";
+            employeeModel.EmployeeName = "Shiv";
             employeeModel.PhoneNumber = "9685632154";
-            employeeModel.Address = "Banglore";
+            employeeModel.Address = "134-25-66";
             employeeModel.Department = "Hr";
-            employeeModel.Gender = 'F';
-            employeeModel.BasicPay = 3400000.00;
+            employeeModel.Gender = 'M';
+            employeeModel.BasicPay = 5400000.00;
             employeeModel.Deductions = 2500.00;
             employeeModel.TaxablePay = 200.00;
             employeeModel.Tax = 300.00;
             //employeeModel.EmployeeID = 1;
             employeeModel.NetPay = 250000.00;
-            employeeModel.City = "Banglore";
+            employeeModel.City = "Hydrabad";
             employeeModel.Country = "India";
             employeeModel.StartDate = DateTime.Now;
             employeeRepo.AddEmployee(employeeModel);
